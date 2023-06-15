51d7983d81b2aad90effe2b3a32f1916448b2463
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-06-15 12:02:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,13 @@ bot = Client(
     "myfirs",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="6199159516:AAF62NQbzVB3bWm79sgzOHO8BG4ND5dBMLU"
+    bot_token="6032076608:AAEwM7ZpCKKXIdw5TVJVu5jxND2vklkog2U"
 )
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
     bot.send_message(message.chat.id, " السلام عليكم أنا فصل الموسيقا , فقط أرسل الفيديو هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
     
-@bot.on_message(filters.private & filters.incoming & filters.video )
+@bot.on_message(filters.private & filters.incoming & filters.voice )
 def _telegram_file(client, message):
   try: 
     with open("./downloads/entry", 'r') as fh:
@@ -22,22 +22,17 @@ def _telegram_file(client, message):
     pass  
   user_id = message.from_user.id 
   sent_message = message.reply_text('جار الفصل \n\n قال رسول الله ﷺ  لَيَكونَنَّ مِن أُمَّتي أقْوامٌ يَسْتَحِلُّونَ الحِرَ والحَرِيرَ، والخَمْرَ والمَعازِفَ، ولَيَنْزِلَنَّ أقْوامٌ إلى جَنْبِ عَلَمٍ، يَرُوحُ عليهم بسارِحَةٍ لهمْ، يَأْتِيهِمْ -يَعْنِي الفقِيرَ- لِحاجَةٍ، فيَقولونَ: ارْجِعْ إلَيْنا غَدًا، فيُبَيِّتُهُمُ اللَّهُ، ويَضَعُ العَلَمَ، ويَمْسَخُ آخَرِينَ قِرَدَةً وخَنازِيرَ إلى يَومِ القِيامَةِ. ( صحيح البخاري)', quote=True)
-  file = message.video
+  file = message.voice
   file_path = message.download(file_name="entry")
 
     # Execute speech.py script with entry file
   subprocess.call(['ffmpeg', '-i',"./downloads/entry",'-q:a','0','-map','a',"entry.mp3",'-y' ])
-  subprocess.call(['ffmpeg','-i',"./downloads/entry",'-c','copy','-an',"entry.mp4",'-y'])
-  subprocess.call(['spleeter', 'separate', '-p', 'spleeter:2stems', '-o', 'output' , "entry.mp3" ])
-  subprocess.call(['mv',"./output/entry/vocals.wav" , "./output/entry/vocals.mp3" ])
-  subprocess.call(['ffmpeg', '-i',"entry.mp4",'-i',"./output/entry/vocals.mp3",'-c:v','copy','-c:a','aac','output.mp4','-y' ])
     # Upload transcription file to user
-  with open("./output.mp4", 'rb') as f:
-        bot.send_video(message.chat.id, f)
+  with open("./output.mp3", 'rb') as f:
+        bot.send_audio(message.chat.id, f)
   subprocess.call(['sudo','rm','-r',"./downloads/entry"]) 
-  subprocess.call(['sudo','rm','-r',"entry.mp4"]) 
   subprocess.call(['sudo','rm','-r',"entry.mp3"]) 
-  subprocess.call(['sudo','rm','-r',"output.mp4"]) 
+ 
  
  
 
