b22fe0d135d0aafd74c09a946697e102c1ab5425
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-09-30 22:42:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = Client(
     "audiobot",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="6032076608:AAGhqffAlibHd7pipzA3HR2-0Ca3sDFlmdI"
+    bot_token="6030811502:AAF0tj9q_2BH1HpmRZLkuvBQttmxYdYFw6o"
 )
 
 CHOOSE_UR_AUDIO_MODE = "اختر العملية  التي تريد "
