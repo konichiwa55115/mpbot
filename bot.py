472fe2b325d6cc1f6401a76ecd92e6413c8d80f4
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2024-02-03 18:12:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,12 +9,31 @@ audmergelist = []
 vidmergelist = []
 queeq = []   
 temptxt = "res.txt"
-from pyrogram import Client, filters 
+from oauth2client.file import Storage
+from httplib2 import HttpLib2Error
+from oauth2client.client import (
+    OAuth2WebServerFlow,
+    FlowExchangeError,
+    OAuth2Credentials,
+)
+from http.client import (
+    NotConnected,
+    IncompleteRead,
+    ImproperConnectionState,
+    CannotSendRequest,
+    CannotSendHeader,
+    ResponseNotReady,
+    BadStatusLine,
+)
+
+from apiclient import http, errors, discovery
+from pyrogram import Client, filters,enums,StopTransmission
 from zipfile import ZipFile 
-import os ,re , random ,shutil,asyncio ,pytesseract,requests
+import os ,re , random ,shutil,asyncio ,pytesseract,requests,logging,time,string,datetime,httplib2
+from typing import Optional, Tuple, Union
 from os import system as cmd
 from youtube_transcript_api import YouTubeTranscriptApi
-from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery , ForceReply
+from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery , ForceReply,Message
 import pypdfium2 as pdfium
 from yt_dlp import YoutubeDL
 from PyPDF2 import PdfWriter, PdfReader
@@ -36,6 +55,331 @@ bot = Client(
 #6709809460:AAGWWXJBNMF_4ohBNRS22Tg0Q3-vkm376Eo
 #6466415254:AAE_m_mYGHFuu3MT4T0qzqVCm0WvR4biYvM
 #6812722455:AAEjCb1ZwgBa8DZ4_wVNNjDZbe6EtQZOUxo
+
+@bot.on_message(filters.command('apiswitch1') & filters.private)
+def command2(bot,message):
+   global CLIENT_ID,CLIENT_SECRET
+   CLIENT_ID = "389708947332-oienmum8v600cegsnhb6puk4prsv3pf6.apps.googleusercontent.com"
+   CLIENT_SECRET = "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"
+   message.reply_text("تم ضبط الـapi ")
+@bot.on_message(filters.command('apiswitch2') & filters.private)
+def command2(bot,message):
+   global CLIENT_ID,CLIENT_SECRET
+   CLIENT_ID = "664256487809-21lnbeqr7cau7fng78oeli1bnqcjthvp.apps.googleusercontent.com"
+   CLIENT_SECRET = "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"
+   message.reply_text("تم ضبط الـapi ")
+
+
+
+async def progress(
+    cur: Union[int, float],
+    tot: Union[int, float],
+    start_time: float,
+    status: str,
+    snt: Message,
+    c: bot,
+    download_id: str,
+):
+    if not c.download_controller.get(download_id):
+        raise StopTransmission
+
+    try:
+        diff = int(time.time() - start_time)
+
+        if (int(time.time()) % 5 == 0) or (cur == tot):
+            await asyncio.sleep(1)
+            speed, unit = human_bytes(cur / diff, True)
+            curr = human_bytes(cur)
+            tott = human_bytes(tot)
+            eta = datetime.timedelta(seconds=int(((tot - cur) / (1024 * 1024)) / speed))
+            elapsed = datetime.timedelta(seconds=diff)
+            progress = round((cur * 100) / tot, 2)
+            text = f"{status}\n\n{progress}% done.\n{curr} of {tott}\nSpeed: {speed} {unit}PS"
+            f"\nETA: {eta}\nElapsed: {elapsed}"
+            await snt.edit_text(
+                text=text,
+                reply_markup=InlineKeyboardMarkup(
+                    [[InlineKeyboardButton("Cancel!", f"cncl+{download_id}")]]
+                ),
+            )
+
+    except Exception as e:
+        pass
+class MaxRetryExceeded(Exception):
+    pass
+
+
+class UploadFailed(Exception):
+    pass
+
+
+class YouTube:
+
+    MAX_RETRIES = 10
+
+    RETRIABLE_EXCEPTIONS = (
+        HttpLib2Error,
+        IOError,
+        NotConnected,
+        IncompleteRead,
+        ImproperConnectionState,
+        CannotSendRequest,
+        CannotSendHeader,
+        ResponseNotReady,
+        BadStatusLine,
+    )
+
+    RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+
+    def __init__(self, auth: discovery.Resource, chunksize: int = -1):
+        self.youtube = auth
+        self.request = None
+        self.chunksize = chunksize
+        self.response = None
+        self.error = None
+        self.retry = 0
+
+    def upload_video(
+        self, video: str, properties: dict, progress: callable = None, *args
+    ) -> dict:
+        self.progress = progress
+        self.progress_args = args
+        self.video = video
+        self.properties = properties
+
+        body = dict(
+            snippet=dict(
+                title=self.properties.get("title"),
+                description=self.properties.get("description"),
+                categoryId=self.properties.get("category"),
+            ),
+            status=dict(privacyStatus=self.properties.get("privacyStatus")),
+        )
+
+        media_body = http.MediaFileUpload(
+            self.video,
+            chunksize=self.chunksize,
+            resumable=True,
+        )
+
+        self.request = self.youtube.videos().insert(
+            part=",".join(body.keys()), body=body, media_body=media_body
+        )
+        self._resumable_upload()
+        return self.response
+
+    def _resumable_upload(self) -> dict:
+        response = None
+        while response is None:
+            try:
+                status, response = self.request.next_chunk()
+                if response is not None:
+                    if "id" in response:
+                        self.response = response
+                    else:
+                        self.response = None
+                        raise UploadFailed(
+                            "The file upload failed with an unexpected response:{}".format(
+                                response
+                            )
+                        )
+            except errors.HttpError as e:
+                if e.resp.status in self.RETRIABLE_STATUS_CODES:
+                    self.error = "A retriable HTTP error {} occurred:\n {}".format(
+                        e.resp.status, e.content
+                    )
+                else:
+                    raise
+            except self.RETRIABLE_EXCEPTIONS as e:
+                self.error = "A retriable error occurred: {}".format(e)
+
+            if self.error is not None:
+                self.retry += 1
+
+                if self.retry > self.MAX_RETRIES:
+                    raise MaxRetryExceeded("No longer attempting to retry.")
+
+                max_sleep = 2 ** self.retry
+                sleep_seconds = random.random() * max_sleep
+                time.sleep(sleep_seconds)
+class Uploader:
+    def __init__(self, file: str, title: Optional[str] = None):
+        self.file = file
+        self.title = title
+        self.video_category = {
+            1: "Film & Animation",
+            2: "Autos & Vehicles",
+            10: "Music",
+            15: "Pets & Animal",
+            17: "Sports",
+            19: "Travel & Events",
+            20: "Gaming",
+            22: "People & Blogs",
+            23: "Comedy",
+            24: "Entertainment",
+            25: "News & Politics",
+            26: "Howto & Style",
+            27: "Education",
+            28: "Science & Technology",
+            29: "Nonprofits & Activism",
+        }
+
+    async def start(self, progress: callable = None, *args) -> Tuple[bool, str]:
+        self.progress = progress
+        self.args = args
+
+        await self._upload()
+
+        return self.status, self.message
+
+    async def _upload(self) -> None:
+        try:
+            loop = asyncio.get_running_loop()
+
+            auth = GoogleAuth(CLIENT_ID, CLIENT_SECRET)
+
+            if not os.path.isfile("auth_token.txt"):
+                self.status = False
+                self.message = "أنت لم تقم بالمصادقة بعد !."
+                return
+
+            auth.LoadCredentialsFile("auth_token.txt")
+            google = await loop.run_in_executor(None, auth.authorize)
+            categoryId = 27
+            categoryName = self.video_category[categoryId]
+            title = self.title if self.title else os.path.basename(self.file)
+            title = (
+                (title)
+                .replace("<", "")
+                .replace(">", "")[:100]
+            )
+            description = (""+ "\n")[:5000]
+            privacyStatus = "private"
+            properties = dict(
+                title=title,
+                description=description,
+                category=categoryId,
+                privacyStatus=privacyStatus,
+            )
+            youtube = YouTube(google)
+            r = await loop.run_in_executor(
+                None, youtube.upload_video, self.file, properties
+            )
+
+
+            video_id = r["id"]
+            self.status = True
+            self.message = (
+                f"[{title}](https://youtu.be/{video_id}) uploaded to YouTube under category "
+                f"{categoryId} ({categoryName})"
+            )
+        except Exception as e:
+            self.status = False
+            self.message = f"Error occuered during upload.\nError details: {e}"
+
+
+
+class AuthCodeInvalidError(Exception):
+    pass
+
+
+class InvalidCredentials(Exception):
+    pass
+
+
+class NoCredentialFile(Exception):
+    pass
+
+
+class GoogleAuth:
+    OAUTH_SCOPE = ["https://www.googleapis.com/auth/youtube.upload"]
+    REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
+    API_SERVICE_NAME = "youtube"
+    API_VERSION = "v3"
+
+    def __init__(self, CLIENT_ID: str, CLIENT_SECRET: str):
+        self.flow = OAuth2WebServerFlow(
+            CLIENT_ID, CLIENT_SECRET, self.OAUTH_SCOPE, redirect_uri=self.REDIRECT_URI
+        )
+        self.credentials: Optional[OAuth2Credentials] = None
+
+    def GetAuthUrl(self) -> str:
+        return self.flow.step1_get_authorize_url()
+
+    def Auth(self, code: str) -> None:
+        try:
+            self.credentials = self.flow.step2_exchange(code)
+        except FlowExchangeError as e:
+            raise AuthCodeInvalidError(e)
+        except Exception:
+            raise
+
+    def authorize(self):
+        try:
+            if self.credentials:
+                http = httplib2.Http()
+                self.credentials.refresh(http)
+                http = self.credentials.authorize(http)
+                return discovery.build(
+                    self.API_SERVICE_NAME, self.API_VERSION, http=http
+                )
+            else:
+                raise InvalidCredentials("No credentials!")
+        except Exception:
+            raise
+
+    def LoadCredentialsFile(self, cred_file: str) -> None:
+        if not os.path.isfile(cred_file):
+            raise NoCredentialFile(
+                "No credential file named {} is found.".format(cred_file)
+            )
+        storage = Storage(cred_file)
+        self.credentials = storage.get()
+
+    def SaveCredentialsFile(self, cred_file: str) -> None:
+        storage = Storage(cred_file)
+        storage.put(self.credentials)
+@bot.on_message( filters.private & filters.incoming & filters.command("authhelp"))
+async def _help(bot,message):
+    auth = GoogleAuth(CLIENT_ID,CLIENT_SECRET)
+    url = auth.GetAuthUrl()
+    button = [
+            [InlineKeyboardButton(text="Authentication URL", url=url)],
+        ]
+    await message.reply_text(
+        text="الآن قم بتفعيل الـapi",
+        reply_markup=InlineKeyboardMarkup(button),
+    )
+
+
+help_callback_filter = filters.create(
+    lambda _, __, query: query.data.startswith("help+")
+)
+
+
+@bot.on_message(filters.private & filters.incoming & filters.command("authorise"))
+async def _auth(c: bot, m: Message) -> None:
+    if len(m.command) == 1:
+        await m.reply_text("لم تعطني أي كود !", True)
+        return
+
+    code = m.command[1]
+
+    try:
+        auth = GoogleAuth(CLIENT_ID, CLIENT_SECRET)
+
+        auth.Auth(code)
+
+        auth.SaveCredentialsFile("auth_token.txt")
+
+        msg = await m.reply_text("تمت المصادقة بنجاح ", True)
+        with open("auth_token.txt", "r") as f:
+            cred_data = f.read()
+
+    except Exception as e:
+        await m.reply_text("فشلت المصادقة للأسف", True)
+
+
 def merge_images1(file1, file2):
     image1 = Image.open(file1)
     image2 = Image.open(file2)
@@ -69,7 +413,7 @@ CHOOSE_UR_AUDIO_MODE_BUTTONS = [
     [InlineKeyboardButton("تسريع ",callback_data="speedy"),InlineKeyboardButton("تحويل ",callback_data="conv"),InlineKeyboardButton("تفريغ ",callback_data="transcribe")], 
     [InlineKeyboardButton("دمج  ",callback_data="audmerge"),InlineKeyboardButton("إعادة التسمية ",callback_data="renm")],
     [InlineKeyboardButton("تغيير الصوت",callback_data="voicy"),InlineKeyboardButton("تقسيم الصوتية ",callback_data="splitty"),InlineKeyboardButton("إزالة الصمت",callback_data="rmvsilence")],
-    [InlineKeyboardButton("عكس pdf",callback_data="reversepdf"),InlineKeyboardButton("الرفع لأرشيف",callback_data="upldarch")],
+    [InlineKeyboardButton("عكس pdf",callback_data="reversepdf"),InlineKeyboardButton("الرفع لأرشيف",callback_data="upldarch"),InlineKeyboardButton("الرفع ليوتيوب",callback_data="upldtout")],
     [InlineKeyboardButton("ضغط الملفات ",callback_data="zipfile"),InlineKeyboardButton("استخراج",callback_data="unzip")],
     [InlineKeyboardButton(" ترجمة + فيديو",callback_data="vidsrt"),InlineKeyboardButton("تغيير الأبعاد  ",callback_data="vidasp"),InlineKeyboardButton("منتجة فيديو ",callback_data="imagetovid")],
     [InlineKeyboardButton("إبدال صوت الفيديو ",callback_data="subs"),InlineKeyboardButton("صورة إلى gif",callback_data="imagetogif"),InlineKeyboardButton("كتم الصوت ",callback_data="mute")]
@@ -374,6 +718,7 @@ async def _telegram_file(client, message):
  mergdir = f"./mergy/{mp3file}"
  trimdir = f"./trimmo/{mp3file}" 
  result = f"{nom}.txt"    
+ videoupldtitle = nepho.caption
  @bot.on_callback_query()
  async def callback_query(CLIENT,CallbackQuery): 
   global amplemode
@@ -1125,7 +1470,17 @@ async def _telegram_file(client, message):
       sentfile = f"{unzippath}{images[x]}"
       await bot.send_document(user_id,sentfile)
     shutil.rmtree(unzippath)
-
+  elif  CallbackQuery.data == "upldtout" :
+    if user_id==6234365091 :
+         upload = Uploader(file_path,videoupldtitle )
+         snt = await CallbackQuery.edit_message_text("جار الرفع")
+         status, link = await upload.start(progress, snt)
+         await snt.edit_text(text=link, parse_mode=enums.ParseMode.MARKDOWN)
+         os.remove(file_path)
+    else :
+         await CallbackQuery.edit_message_text("هذه الميزة متوفرة لمالك البوت فقط")
+    
+ 
 
      
   queeq.clear()
